c8deb400a4b8233f38587db41f01c42805eecc25
nasirMasud <nasir.masud@ymail.com>
2023-12-22 20:19:33 +0600

    Displaying critic score

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,18 @@
+import { Badge } from '@chakra-ui/react';
+
+interface Props {
+  score: number;
+}
+
+const CriticScore = ({ score }: Props) => {
+  const color =
+    score > 85 ? 'lime' : score > 70 ? 'yellow' : score < 60 ? 'red' : '';
+
+  return (
+    <Badge color={color} fontSize="14px" paddingX={2} borderRadius="4px">
+      {score}
+    </Badge>
+  );
+};
+
+export default CriticScore;

1a73c5c7279120aef3ff9e93f17759441edd9167
nasirMasud <nasir.masud@ymail.com>
2023-12-23 15:49:29 +0600

    Fix Layout & Genre Icon.

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,14 +1,26 @@
+import { HStack, Image, List, ListItem, Text } from '@chakra-ui/react';
 import useGenre from '../hooks/useGenre';
+import getCroppedImageUrl from '../services/image-url';
 
 const GenreList = () => {
   const { data } = useGenre();
 
   return (
-    <ul>
+    <List>
       {data.map((genre) => (
-        <li key={genre.id}>{genre.name}</li>
+        <ListItem key={genre.id}>
+          <HStack>
+            <Image
+              margin={1}
+              boxSize="32px"
+              borderRadius={8}
+              src={getCroppedImageUrl(genre.image_background)}
+            />
+            <Text>{genre.name}</Text>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
 

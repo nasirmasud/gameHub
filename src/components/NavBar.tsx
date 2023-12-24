1f5f071aa6dd560b69e0050940630a58104ee52d
nasirMasud <nasir.masud@ymail.com>
2023-12-24 20:12:12 +0600

    Implement searching

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,13 @@
 import { HStack, Image } from '@chakra-ui/react';
 import logoD from '../assets/logo-d.webp';
-import SearchInput from './SearchInput';
+import SearchInput, { Props } from './SearchInput';
 import ThemeSwitcher from './ThemeSwitcher';
 
-const NavBar = () => {
+const NavBar = ({ onSearch }: Props) => {
   return (
     <HStack padding="10px">
       <Image src={logoD} boxSize="64px" />
-      <SearchInput />
+      <SearchInput onSearch={onSearch} />
       <ThemeSwitcher />
     </HStack>
   );
